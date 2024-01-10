5e594fe8797eff310b79bd38c18bb84695e05fe7
ingaikvi <vikas.gaikwad@ifm.com>
2024-01-10 14:45:19 +0530

    Ported getmac example to v1.4.3

--- o3d3xx-o3x1xx/getmac/ex-getmac.cpp
+++ o3d3xx-o3x1xx/getmac/ex-getmac.cpp
@@ -1,18 +1,6 @@
 /*
- * Copyright (C) 2016 Love Park Robotics, LLC
- * Copyright (C) 2017 ifm syntron gmbh
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distribted on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2022-present ifm electronic, gmbh
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 //
@@ -23,20 +11,20 @@
 //
 
 #include <iostream>
-#include <ifm3d/camera.h>
+#include <ifm3d/device/device.h>
 
 int main(int argc, const char **argv)
 {
   // get access to the camera
-  auto cam = ifm3d::Camera::MakeShared();
+  auto cam = ifm3d::Device::MakeShared();
 
   // get the JSON configuration data from the camera
   auto jsonConfig = cam->ToJSON();
 
   // print out the MAC address
   std::cout << "The MAC address of the camera: "
-            << jsonConfig["ifm3d"]["Net"]["MACAddress"]
-            << std::endl;
+              << jsonConfig["ifm3d"]["Net"]["MACAddress"]
+              << std::endl;
 
   return 0;
 }
