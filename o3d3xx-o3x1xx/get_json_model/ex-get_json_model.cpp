7d54d8cf736be5f3cc9b5150143aad6c6630c168
Vikas Gaikwad <vikas.gaikwad@ifm.com>
2024-01-23 15:34:11 +0530

    Added example to get the JSON_MODEL from the device

---
+++ o3d3xx-o3x1xx/get_json_model/ex-get_json_model.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022-present ifm electronic, gmbh
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//
+// ex-get_json_model.cpp
+//
+// Capture a frame from the camera, to retrive the JOSN_MODEL.
+//
+
+#include <iostream>
+#include <memory>
+#include <string.h>
+
+#include <ifm3d/fg.h>
+#include <ifm3d/device/device.h>
+
+
+int main(int argc, const char **argv)
+{
+  auto cam = ifm3d::Device::MakeShared();
+ 
+  ifm3d::FrameGrabber::Ptr fg = std::make_shared<ifm3d::FrameGrabber>(cam);
+  
+   fg->Start({ifm3d::buffer_id::JSON_MODEL});
+
+  auto frame = fg->WaitForFrame();
+  
+  if (frame.wait_for(std::chrono::milliseconds(1000)) != std::future_status::ready)
+    {
+      std::cerr << "Timeout waiting for camera!" << std::endl;
+      return -1;
+    }
+
+  auto json_model = frame.get()->GetBuffer(ifm3d::buffer_id::JSON_MODEL);
+  
+  std::string json = std::string(json_model.ptr<char>(0), strnlen_s(json_model.ptr<char>(0), json_model.size()));
+  std::cout << "JSON MODEL : \n" << json << std::endl;
+   
+  return 0;
+}
