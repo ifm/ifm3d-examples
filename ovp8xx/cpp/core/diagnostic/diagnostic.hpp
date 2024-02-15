ba089c3370ce5395f92c6f14f039f848b95a7589
Masson, Lola <lola.masson@ifm.com>
2024-02-15 15:59:17 -0500

    Cleanup header file, enable use of custom callback, pretty print JSON

--- ovp8xx/cpp/core/diagnostic/diagnostic.hpp
+++ ovp8xx/cpp/core/diagnostic/diagnostic.hpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <fstream>
+#include <functional>
 #include <ifm3d/device/o3r.h>
 #include <ifm3d/fg.h>
 #include <iostream>
@@ -7,74 +8,75 @@ using namespace ifm3d::literals;
 
 class O3RDiagnostic {
 public:
-  ifm3d::O3R::Ptr o3r;
-  ifm3d::FrameGrabber::Ptr fg;
-  std::streambuf *consoleBuffer;
-  bool log_to_file;
-  std::ofstream logFile;
-  ifm3d::json diagnostic =
-      ifm3d::json::parse(R"({"id": "None", "message": "None"})");
-
-  O3RDiagnostic(ifm3d::O3R::Ptr o3r_, bool log_to_file_ = false,
+  O3RDiagnostic(ifm3d::O3R::Ptr o3r, bool log_to_file = false,
                 const std::string &file_name_ = "")
-      : o3r(o3r_), log_to_file(log_to_file_),
-        fg(std::make_shared<ifm3d::FrameGrabber>(o3r, 50009)),
-        consoleBuffer(std::clog.rdbuf()) {
-    if (log_to_file) {
+      : o3r_(o3r), log_to_file_(log_to_file),
+        fg_(std::make_shared<ifm3d::FrameGrabber>(o3r_, 50009)),
+        consoleBuffer_(std::clog.rdbuf()) {
+    if (log_to_file_) {
       const std::string &log_file_name =
           (file_name_.empty()) ? "O3R_diagnostic.txt" : file_name_;
-      if (log_to_file) {
-        logFile.open(log_file_name, std::ios::app); // Open the log file
+      if (log_to_file_) {
+        logFile_.open(log_file_name, std::ios::app); // Open the log file
         // Check if the file opened successfully
-        if (!logFile.is_open()) {
+        if (!logFile_.is_open()) {
           std::cerr << "Failed to open log file: " << log_file_name
                     << std::endl;
           return;
         }
-        std::streambuf *fileBuffer = logFile.rdbuf();
+        std::streambuf *fileBuffer = logFile_.rdbuf();
         // Redirect std::clog to the log file
         std::clog.rdbuf(fileBuffer);
       }
     }
-    std::clog.rdbuf(consoleBuffer);
+    std::clog.rdbuf(consoleBuffer_);
   }
 
   ifm3d::json GetDiagnosticFiltered(ifm3d::json filter_mask_) {
-    try {
-      return o3r->GetDiagnosticFiltered(filter_mask_);
-    } catch (...) {
-      throw;
-    }
+    return o3r_->GetDiagnosticFiltered(filter_mask_);
   }
 
   void StartAsyncDiag() {
-    fg->OnAsyncError([this](int id_, const std::string &message_) {
+    fg_->OnAsyncError([this](int id_, const std::string &message_) {
       this->AsyncDiagCallback_(id_, message_);
     });
-    fg->Start({});
+    fg_->Start({});
   }
 
-  void StopAsyncDiag() { fg->Stop(); }
+  void StartAsyncDiag(std::function<void(int, const std::string&)> callback) {
+    fg_->OnAsyncError(callback);
+    fg_->Start({});
+  }
+
+  void StopAsyncDiag() { fg_->Stop(); }
 
   ~O3RDiagnostic() {
     // Stop the frame grabber if it's still running
-    fg->Stop();
+    fg_->Stop();
 
     // Restore the console output stream buffer
-    std::clog.rdbuf(consoleBuffer);
+    std::clog.rdbuf(consoleBuffer_);
 
     // Close the log file if it's open
-    if (logFile.is_open()) {
-      logFile.close();
+    if (logFile_.is_open()) {
+      logFile_.close();
     }
   }
 
 private:
+  ifm3d::O3R::Ptr o3r_;
+  ifm3d::FrameGrabber::Ptr fg_;
+  std::streambuf *consoleBuffer_;
+  bool log_to_file_;
+  std::ofstream logFile_;
+  ifm3d::json diagnostic_ =
+      ifm3d::json::parse(R"({"id": "None", "message": "None"})");
+  
   void AsyncDiagCallback_(int id_, std::string message_) {
-    diagnostic["id"] = id_;
-    diagnostic["message"] = message_;
+    diagnostic_["id"] = id_;
+    diagnostic_["message"] = message_;
     std::clog << "\n//////////////////////////////////" << std::endl;
-    std::clog << "Id: " << diagnostic["id"] << std::endl;
-    std::clog << "Message: " << diagnostic["message"] << std::endl;
+    std::clog << "Id: " << diagnostic_["id"] << std::endl;
+    std::clog << "Message: " << diagnostic_["message"] << std::endl;
   }
 };
--- ovp8xx/cpp/core/diagnostic/diagnostic_main.cpp
+++ ovp8xx/cpp/core/diagnostic/diagnostic_main.cpp
@@ -1,15 +1,22 @@
 #include "diagnostic.hpp"
 #include <ifm3d/device/o3r.h>
 #include <iostream>
+#include <thread>
 
+using namespace std::chrono_literals;
 using namespace ifm3d::literals;
+
+void CustomCallback(int id_, std::string message_){
+  std::clog << "Custom callback: " << id_ << " " << message_ << std::endl;
+}
 
 int main() {
   auto o3r = std::make_shared<ifm3d::O3R>();
   // To log to file, use diagnostic(o3r, true, "file_name").
   // Log outputs will be redirected both to the file and
   // to the console.
-  O3RDiagnostic diagnostic(o3r);
+  auto log_to_file = false;
+  O3RDiagnostic diagnostic(o3r, log_to_file);
 
   ////////////////////////////////////////////////
   // Examples on how to retrieve the diagnostic
@@ -17,12 +24,12 @@ int main() {
   ////////////////////////////////////////////////
   // Using ifm3d::json::object()
   std::clog << "All current diagnostics:\n"
-            << diagnostic.GetDiagnosticFiltered(ifm3d::json::object())
+            << diagnostic.GetDiagnosticFiltered(ifm3d::json::object()).dump(4)
             << std::endl;
 
   std::clog << "Active diagnostics:\n"
             << diagnostic.GetDiagnosticFiltered(
-                   ifm3d::json::parse(R"({"state": "active"})"))
+                   ifm3d::json::parse(R"({"state": "active"})")).dump(4)
             << std::endl
             << std::endl
             << std::endl;
@@ -32,15 +39,13 @@ int main() {
   // and display errors for "d" seconds.
   ////////////////////////////////////////////////
 
-  diagnostic.StartAsyncDiag();
+  diagnostic.StartAsyncDiag(CustomCallback);
+  // You can also call the function without a custom 
+  // callback, in which case the default callback will
+  // be used:
+  // diagnostic.StartAsyncDiag();
 
-  // Loop for duration "d"
-  int d = 5;
-  for (auto start = std::chrono::steady_clock::now(), now = start;
-       now < start + std::chrono::seconds{d};
-       now = std::chrono::steady_clock::now()) {
-    // Do nothing, just wait for the specified duration
-  }
+  std::this_thread::sleep_for(20s);
 
   diagnostic.StopAsyncDiag();
 
