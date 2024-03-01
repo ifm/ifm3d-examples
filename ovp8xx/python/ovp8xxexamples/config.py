7ceb383e1788d669df05fadfb2419e8baa62969d
Masson, Lola <lola.masson@ifm.com>
2024-03-01 08:16:20 -0500

    Revert "added paramiko key_gen demo"
    This reverts commit 821208db0fe1fd5a01e974789d78b2792ed5b188.

--- ovp8xx/python/ovp8xxexamples/config.py
+++ ovp8xx/python/ovp8xxexamples/config.py
@@ -1,14 +1,12 @@
 # Configurations for the OVP8xx examples.
 # Edit this file to match your setup.
-from pathlib import Path
-import os
-
-CURRENT_DIR = Path(__file__).parent.resolve().as_posix()
+import pathlib
+CURRENT_DIR = pathlib.Path(__file__).parent.resolve().as_posix()
 
 ############################################
 # Device configuration
 ############################################
-IP: str = os.environ.get("IFM3D_IP", "192.168.0.69")
+IP: str = "192.168.0.69"
 PORT_2D: str = "port0"
 PORT_3D: str = "port2"
 
