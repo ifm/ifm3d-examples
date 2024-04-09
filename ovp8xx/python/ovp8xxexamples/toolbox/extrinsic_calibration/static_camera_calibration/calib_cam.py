a2a7387481596022014548c6cd56efadb5575868
Fares <fares.kchaou@ifm.com>
2024-04-09 15:06:13 +0200

    rephrased upside down explanation

--- ovp8xx/python/ovp8xxexamples/toolbox/extrinsic_calibration/static_camera_calibration/calib_cam.py
+++ ovp8xx/python/ovp8xxexamples/toolbox/extrinsic_calibration/static_camera_calibration/calib_cam.py
@@ -38,9 +38,11 @@ ip: str = "192.168.0.69"
 # Horizontal: camera's long side is parallel to the floor plane
 # Set horizontal_mounting = False when the camera is mounted vertically
 horizontal_mounting: bool = True
-# Right side up:
-# label is on the top side of the camera if the camera is mounted horizontally,
-# cable is pointing down if the camera is mounted vertically
+
+# Set upside_down = False when:
+# The camera is mounted horizontally: label is on the top side of the camera or
+# The camera is mounted vertically: cable is pointing down
+# else set upside_down = True
 upside_down: bool = False
 
 logger.info(f"Camera port: {cam_port}")
