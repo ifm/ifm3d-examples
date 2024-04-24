35a10e3960506b48bdcab041a645161cbfa2505c
Masson, Lola <lola.masson@ifm.com>
2024-04-24 11:22:56 -0400

    Use logging, and only print out first sample of imu

--- ovp8xx/python/ovp8xxexamples/core/imu_data.py
+++ ovp8xx/python/ovp8xxexamples/core/imu_data.py
@@ -5,8 +5,11 @@
 
 # Example script to receive the data from IMU
 # %%
+import logging
+
 from ifm3dpy.device import O3R
 from ifm3dpy.framegrabber import FrameGrabber, buffer_id
+
 try:
     from ovp8xxexamples.core.deserialize_imu import IMUOutput
 except ImportError:
@@ -15,6 +18,7 @@ except ImportError:
     print("Defaulting to the local import.")
     from deserialize_imu import IMUOutput
 # %%
+logger = logging.getLogger(__name__)
 
 
 def main(ip: str, port_imu: str):
@@ -46,22 +50,31 @@ def main(ip: str, port_imu: str):
     imu_data_raw = frame.get_buffer(buffer_id.O3R_RESULT_IMU)
     # at the moment ifm3dpy only pass the raw data from the pcic port
     imu_data = IMUOutput.parse(imu_data_raw)
-    print(f"IMU version: {imu_data.imu_version}")
-    print(f"Number of Samples: {imu_data.num_samples}")
-    for i in range(len(imu_data.imu_samples)):
-        print(f"Sample {i}: {imu_data.imu_samples[i]} \n")
-    print(
+    logger.info(f"IMU version: {imu_data.imu_version}\n")
+    logger.info(f"Number of Samples: {imu_data.num_samples}\n")
+    logger.info(f"First sample:")
+    logger.info(f"    Hardware timestamp: {imu_data.imu_samples[0].hw_timestamp}")
+    logger.info(f"    Acquisition timestamp: {imu_data.imu_samples[0].timestamp}")
+    logger.info(f"    Temperature: {imu_data.imu_samples[0].temperature}")
+    logger.info(
+        f"    Acceleration: \n        x: {imu_data.imu_samples[0].accel_x} \n        y: {imu_data.imu_samples[0].accel_y} \n        z: {imu_data.imu_samples[0].accel_z}"
+    )
+    logger.info(
+        f"    Gyroscope: \n        x: {imu_data.imu_samples[0].gyro_x} \n        y: {imu_data.imu_samples[0].gyro_y} \n        z: {imu_data.imu_samples[0].gyro_z}\n"
+    )
+    logger.info(
         f"Extrinsic IMU to User: \n rot_x: {imu_data.extrinsic_imu_to_user.rot_x} \n rot_y: {imu_data.extrinsic_imu_to_user.rot_y} \n rot_z: {imu_data.extrinsic_imu_to_user.rot_z} \n trans_x: {imu_data.extrinsic_imu_to_user.trans_x} \n trans_y: {imu_data.extrinsic_imu_to_user.trans_y} \n trans_z: {imu_data.extrinsic_imu_to_user.trans_z} \n "
     )
-    print(
+    logger.info(
         f"Extrinsic IMU to VPU: \n rot_x: {imu_data.extrinsic_imu_to_vpu.rot_x} \n rot_y: {imu_data.extrinsic_imu_to_vpu.rot_y} \n rot_z: {imu_data.extrinsic_imu_to_vpu.rot_z} \n trans_x: {imu_data.extrinsic_imu_to_vpu.trans_x} \n trans_y: {imu_data.extrinsic_imu_to_vpu.trans_y} \n trans_z: {imu_data.extrinsic_imu_to_vpu.trans_z} \n "
     )
-    print(f"Receive Timestamp: {imu_data.imu_fifo_rcv_timestamp}")
+    logger.info(f"Receive Timestamp: {imu_data.imu_fifo_rcv_timestamp}")
 
     fg.stop().wait()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
     try:
         # If the example python package was build, import the configuration
         from ovp8xxexamples import config
@@ -78,4 +91,4 @@ if __name__ == "__main__":
         IP = "192.168.0.69"
         PORT_IMU = "port6"
 
-    main(ip = IP, port_imu = PORT_IMU)
+    main(ip=IP, port_imu=PORT_IMU)
