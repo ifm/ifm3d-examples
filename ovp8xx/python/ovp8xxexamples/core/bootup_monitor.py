fb1153a164cf2295c8e85560bb20b1d1fa771a4d
Patan, Rasheed Khan <rasheed.khan.patan@ifm.com>
2024-11-08 08:08:14 +0000

    Add logic to wait for diagnostic service

--- ovp8xx/python/ovp8xxexamples/core/bootup_monitor.py
+++ ovp8xx/python/ovp8xxexamples/core/bootup_monitor.py
@@ -6,6 +6,7 @@
 
 import logging
 import time
+import socket
 
 from ifm3dpy.device import O3R
 from ifm3dpy.device import Error as ifm3dpy_error
@@ -17,21 +18,37 @@ class BootUpMonitor:
     def __init__(
         self,
         o3r: O3R,
-        timeout: int = 25,
+        timeout: int = 60,
         wait_time: int = 0.5,
     ) -> None:
         self.o3r = o3r
         self._stages = ["device", "ports", "applications"]
+        self._diagnostics_PCICPort = 50009
         self.timeout = timeout
         self.wait_time = wait_time
         self._ip = o3r.ip
 
         self.logger = logging.getLogger(__name__)
-        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+    def wait_for_diagnostics(self, retry_delay=0.5):
+        """Waits until the diagnostics service is available at the given IP and port."""
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                # Optional: set timeout to avoid indefinite hanging
+                sock.settimeout(1)
+                result = sock.connect_ex(
+                    (self._ip, self._diagnostics_PCICPort))
+                if result == 0:
+                    self.logger.info("Diagnostics service available.")
+                    return
+            time.sleep(retry_delay)
 
     def retrieve_boot_diagnostic(self):
+        self.logger.info("Retrieving diagnostics: \n")
         for error in self.o3r.get_diagnostic_filtered({"state": "active"})["events"]:
-            self.logger.warning("Active errors: %s, %s", error["id"], error["name"])
+            self.logger.warning("Active errors: %s, %s",
+                                error["id"], error["name"])
 
     def monitor_VPU_bootup(self) -> bool:
         """
@@ -43,7 +60,7 @@ class BootUpMonitor:
 
         Args:
             o3r (ifm3dpy.O3R): O3R handle
-            timeout (int, optional): Maximum time expected for bootup sequence in seconds. Defaults to 25.
+            timeout (int, optional): Maximum time expected for bootup sequence in seconds. Defaults to 60.
             wait_time (int, optional): how long to pause between queries to the VPU. Defaults to .5.
 
         Raises:
@@ -68,6 +85,7 @@ class BootUpMonitor:
             if config:
                 confInitStages = config["device"]["diagnostic"]["confInitStages"]
                 if all(x in self._stages for x in confInitStages):
+                    self.wait_for_diagnostics(retry_delay=0.5)
                     self.logger.info("VPU fully booted.")
                     self.retrieve_boot_diagnostic()
                     return True
